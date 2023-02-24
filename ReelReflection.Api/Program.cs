cf95ea86db13a42aabcfb7cc0386d54a81a80803
ditadev <abrahaminyaka@yahoo.com>
2023-02-24 02:37:13 +0100

    Refactored

--- ReelReflection.Api/Program.cs
+++ ReelReflection.Api/Program.cs
@@ -10,11 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(myAllowSpecificOrigins,
+        policy => { policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); });
+});
+
 IConfigurationRoot? config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", false, true)
     .AddEnvironmentVariables()
     .Build();
+
 var appSettings = config.GetSection("AppSettings").Get<AppSettings>();
+
 builder.Services.AddSingleton(appSettings);
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IMovieService, MovieService>();
@@ -27,6 +37,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
